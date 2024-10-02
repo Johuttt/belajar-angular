a8ba7a19205cdebe65c7bc2eb6f79bd1bc027210
Jonatan Hutahaean <2010631170149@student.unsika.ac.id>
2024-10-02 08:10:26 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'belajar_angular';
+
+  dummyData = {
+    title: 'My First Project',
+    content: 'This is my first angular application project',
+  };
+
+  internalMessage: string = 'This is my internal message app.component';
+
+  receiveMessage($message: string): void {
+    this.internalMessage = $message;
+  }
 }
